c7ce541a5530d51925f85c61409ec5d8e978b8f0
PP <1940506532@qq.com>
2021-04-19 23:11:42 +0800

    4/19配置

---
+++ src/test/java/com/pp/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pp.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
