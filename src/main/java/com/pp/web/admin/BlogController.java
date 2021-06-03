c2aa559a1af257d903343a3c7ddbc925d5a1dea7
PP <1940506532@qq.com>
2021-06-03 20:07:04 +0800

    6/3分类管理

--- src/main/java/com/pp/web/admin/BlogController.java
+++ src/main/java/com/pp/web/admin/BlogController.java
@@ -1,2 +1,17 @@
-package com.pp.web.admin;public class BlogController {
+package com.pp.web.admin;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class BlogController {
+
+    @GetMapping("/blogs")
+    public String list(){
+        return "admin/blogs";
+    }
+
 }
